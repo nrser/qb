805ea24c03feb8abd6de0586ceccb74734496256
nrser <neil@ztkae.com>
2016-02-11 16:13:25 -0800

    gem role /spec dir

---
+++ spec/qb_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe QB do
+  it 'has a version number' do
+    expect(QB::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
