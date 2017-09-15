34f945ca31000da57261d2e68e50846e471f2aaf
nrser <neil@ztkae.com>
2017-09-16 02:13:24 +0800

    Start plushing out the play cmd based on QB::Ansible::Cmds::Playbook

--- lib/qb/cli/play.rb
+++ lib/qb/cli/play.rb
@@ -1,3 +1,9 @@
+# Requirements
+# =====================================================================
+
+# package
+require 'qb/ansible/cmds/playbook'
+
 
 module QB; end
 
@@ -18,13 +24,33 @@ module QB::CLI
       raise "Need path to playbook in first arg."
     end
     
-    path = QB::Util.resolve args[0]
+    playbook_path = QB::Util.resolve args[0]
     
-    unless path.file?
+    unless playbook_path.file?
       raise "Can't find Ansible playbook at #{ path.to_s }"
     end
     
+    # By default, we won't change directories to run the command.
+    chdir = nil
     
+    # See if there is an Ansible config in the parent directories
+    ansible_cfg_path = QB::Util.find_up \
+      QB::Ansible::ConfigFile::FILE_NAME,
+      playbook_path.dirname,
+      raise_on_not_found: false
+    
+    # If we did find an Ansible config, we're going to want to run in that
+    # directory and add it to the role search path so that we merge it's 
+    # values into our env vars (otherwise they would override the config
+    # values).
+    unless ansible_cfg_path.nil?
+      QB::Role::PATH.unshift ansible_cfg_path.dirname
+      chdir = ansible_cfg_path.dirname
+    end
+    
+    cmd = QB::Ansible::Cmds::Playbook.new \
+      playbook_path: playbook_path,
+      chdir: chdir
     
   end # .play
   
